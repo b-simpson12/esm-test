8981bbbf8b69aabfe1f2d436f471508d098dd6aa
BSimpson <b.x.simpson12@gmail.com>
2024-02-22 10:58:05 +1100

    Add dynamic import example

--- apps/dynamic-import/src/main.ts
+++ apps/dynamic-import/src/main.ts
@@ -6,19 +6,16 @@
 import express from 'express';
 import * as path from 'path';
 
-const dynamicLowerCase = (value: string) => import('lodash-es').then(({lowerCase}) => lowerCase(value));
-
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to preset-express!' });
+  res.send({ message: 'Welcome to dynamic-import!' });
 });
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(dynamicLowerCase('TEST'));
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
